cfbc6bd3a3c8803d86e16d4b5eebc25e01ef41f7
ivanan266 <neskovicivana266@gmail.com>
2024-10-09 09:31:33 +0200

    Update config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,26 +3,15 @@ const fs = require('fs-extra');
 const path = require('path');
 
 function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve('cypress\\config', `${file}.json`);
-
+  const pathToConfigFile = path.resolve('cypress/config', `${file}.json`);
 
   if (!fs.existsSync(pathToConfigFile)) {
-    console.log("No custom config file found.");
-    return {};
+      throw new Error(`Custom config file not found at: ${pathToConfigFile}`);
   }
 
-  return fs.readJson(pathToConfigFile)
+  return fs.readJson(pathToConfigFile);
 }
 
-module.exports = defineConfig({
-  projectId: 'fkp3me',
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-
-      const file = config.env.configFile || ''
-      return getConfigurationByFile(file)
-    },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     excludeSpecPattern: "cypress/e2e/other/*.js",
     baseUrl: "http://www.webdriveruniversity.com",
